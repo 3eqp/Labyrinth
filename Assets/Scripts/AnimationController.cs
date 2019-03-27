d26f30a5a710d0b8569afa45fb0559d048a1d6c7
Verem4uk <seregaverem4uk@gmail.com>
2019-03-27 16:40:20 +0300

    Upload project

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+	Animator anim;
+	CharacterController controller;
+    static BasicPlayer Player;
+
+    public Camera cam1;
+    public Camera cam2;
+
+    void Start ()
+    {
+        Player = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<BasicPlayer>();
+        anim = GetComponent<Animator>();
+		controller = GetComponent<CharacterController>();
+		cam1.enabled = true;
+     	//cam2.enabled = false;
+    }
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetBool("walk", true);
+           // MainManager.Audio.LoopPlayStart(Player.GetComponent<AudioSource>());
+
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetBool("walk", false);
+            //MainManager.Audio.LoopPlayStop(Player.GetComponent<AudioSource>());
+            
+        }
+		if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("user_anim");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (!Player.isCrouch)
+            {
+                anim.SetBool("crouch", true);
+                Player.isCrouch = true;
+            }
+            else
+            {
+                anim.SetBool("crouch", false);
+                Player.isCrouch = false;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            anim.SetBool("run", true);
+            Player.playerSpeed *= 2.7f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            anim.SetBool("run", false);
+            Player.playerSpeed /= 2.7f;
+        }
+
+        float x= Input.GetAxis("Horizontal");
+		float z= Input.GetAxis("Vertical");
+
+		if(z!=0) {
+            Vector3 dir = transform.TransformDirection(new Vector3(0f, -3f, z*Player.playerSpeed));
+			controller.Move(dir);
+		}
+		if(x!=0) {
+            transform.Rotate(0f, x * Player.playerRotateSpeed, 0f);
+		}
+		if (Input.GetKeyDown(KeyCode.Q)) {
+            cam1.enabled = !cam1.enabled;
+         	//cam2.enabled = !cam2.enabled;
+        }
+	}
+
+
+
+}
---
+++ Assets/Scripts/BasicPlayer.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlayer : MonoBehaviour
+{
+    Animator anim;
+
+    public bool isNear { get; set; }
+    public Vector3 PositionForIК { get; set; }
+
+    public bool isTake { get; set; }
+    public static bool isDrop { get; set; }
+
+    public GameObject TakenObj { get; set; }
+
+    public bool isCrouch { get; set; }
+
+    public float playerSpeed = 0.15f;
+    public float playerRotateSpeed = 5f;
+
+    float smoothWeight = 0;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        isNear = false;
+        isCrouch = false;
+        isDrop = false;
+    }
+
+    void Update() {
+        OnAnimatorIK();
+        if (isDrop){
+            DropItem();
+        }
+        if (Input.GetMouseButton(0)) {
+            if (isNear) {
+                anim.SetBool("crouch", true);
+            }
+            if (isTake) {
+                DropItem();
+            }
+        }
+
+    }
+
+    void OnAnimatorIK() {
+        if (isNear) {
+            if (smoothWeight < 1) smoothWeight += 0.01f;
+            anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, smoothWeight);
+            anim.SetIKPosition(AvatarIKGoal.LeftHand, PositionForIК);
+            anim.SetLookAtWeight(smoothWeight);
+            anim.SetLookAtPosition(PositionForIК);
+        }
+        else {
+            if (smoothWeight > 0) smoothWeight -= 0.02f;
+            anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, smoothWeight);
+            anim.SetIKPosition(AvatarIKGoal.LeftHand, PositionForIК);
+        }
+
+        if (isTake) {
+            anim.SetLookAtWeight(0f);
+            anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1f);
+            anim.SetIKPosition(AvatarIKGoal.LeftHand, transform.position - transform.right*2 + transform.up + transform.forward);
+        }
+    }
+
+    public void DropItem() {
+        Rigidbody rb = TakenObj.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        rb.AddForce(transform.forward * 20, ForceMode.Impulse);
+        TakenObj.transform.parent = null;
+        isTake = false;
+    }
+
+}
