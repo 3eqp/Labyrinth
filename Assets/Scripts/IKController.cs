b0361cffa41daa74eb79f264790bf111a0d78b19
Verem4uk <seregaverem4uk@gmail.com>
2019-03-29 11:16:45 +0300

    add IKController

---
+++ Assets/Scripts/IKController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKController : MonoBehaviour
+{    
+    Animator anim;
+
+    bool near;
+    Vector3 posForIK;
+    float weight = 0f;
+    
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK()
+    {
+        if (near)
+        {
+            if (weight < 1) weight += 0.01f;
+            anim.SetLookAtWeight(weight);
+            anim.SetLookAtPosition(posForIK);
+        }
+        else if (weight > 0)
+        {
+            weight -= 0.01f;
+            anim.SetLookAtWeight(weight);
+            anim.SetLookAtPosition(posForIK);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)//это триггер перса
+    {
+        if (other.CompareTag("item"))
+        {
+            print("object was detected"); 
+            
+            posForIK = other.transform.position;
+            near = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        near = false;
+    }
+}
---
+++ Assets/Scripts/item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)//это триггер перса
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("object was detected");
+           // anim.SetLookAtWeight(1f);
+            //posForIK = other.transform.position;
+            //near = true;
+        }
+    }
+
+}
