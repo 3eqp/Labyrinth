6620f9b5a86e95175c1c7e44fc5a6d6593432e95
Verem4uk <seregaverem4uk@gmail.com>
2019-03-29 10:30:27 +0300

    new Controller for pers

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    CharacterController controller;
+    float moveSpeed = 0.15f;
+    float rotateSpeed = 5f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        if (z != 0)
+        {
+            Vector3 dir = transform.TransformDirection(new Vector3(0f, -3f, z * moveSpeed));
+            controller.Move(dir);
+        }
+
+        if (x != 0)
+        {
+            transform.Rotate(0f, x * rotateSpeed, 0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed *= 2.7f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed /= 2.7f;
+        }
+
+    }
+}
